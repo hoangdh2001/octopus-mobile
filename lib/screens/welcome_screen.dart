5ef33489aef52466fd5c85e7e9dfa0fcea687272
Đỗ Huy Hoàng <dohuyhoang.se.dev@gmail.com>
2023-01-17 23:57:01 +0700

    - Task #OC-15 [APP] Tạo màn hình welcome

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -10,6 +10,70 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
+        width: double.infinity,
+        height: size.height,
+        decoration: const BoxDecoration(
+          color: Color(0xffdfdeda),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(
+              width: size.width * 0.6,
+              child: TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.white;
+                    }
+                    return const Color(0xff726bb8);
+                  }),
+                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text('Sign up'),
+              ),
+            ),
+            SizedBox(
+              width: size.width * 0.6,
+              child: TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.white70;
+                    }
+                    return Colors.white;
+                  }),
+                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text('Log in'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
